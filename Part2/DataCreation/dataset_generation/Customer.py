eb1378757e12f511799e7a7c015014776d7095b0
gbenderiya <143426706+gbenderiya@users.noreply.github.com>
2023-11-12 22:16:43 -0500

    Update Customer.py

--- Part2/DataCreation/dataset_generation/Customer.py
+++ Part2/DataCreation/dataset_generation/Customer.py
@@ -8,7 +8,7 @@ def generate_data(size):
     for i in range(size):
         Customer.append([i+1,names.get_full_name(),random.randint(18,100),random.randint(1,500),random.uniform(100,10000000)])
     
-    Customer=pd.DataFrame(Customer,columns=["id","Name","Age","CountryCode","Salary"])
+    Customer=pd.DataFrame(Customer,columns=["ID","Name","Age","CountryCode","Salary"])
     Customer.to_csv("Customer.csv",index=False)
     
 def main():
