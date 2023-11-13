4b16132fccf71744f02fd34a6376cece7feea77d
gbenderiya <143426706+gbenderiya@users.noreply.github.com>
2023-11-12 22:14:26 -0500

    Update Customer.py
    change salary type to float

--- Part2/DataCreation/dataset_generation/Customer.py
+++ Part2/DataCreation/dataset_generation/Customer.py
@@ -6,7 +6,7 @@ import random
 def generate_data(size):
     Customer=[]
     for i in range(size):
-        Customer.append([i+1,names.get_full_name(),random.randint(18,100),random.randint(1,500),random.randint(100,10000000)])
+        Customer.append([i+1,names.get_full_name(),random.randint(18,100),random.randint(1,500),random.uniform(100,10000000)])
     
     Customer=pd.DataFrame(Customer,columns=["id","Name","Age","CountryCode","Salary"])
     Customer.to_csv("Customer.csv",index=False)
