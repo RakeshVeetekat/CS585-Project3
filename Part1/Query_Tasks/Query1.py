2f5ab9fa888742e3583bd18ab950629f6d46fd48
RakeshVeetekat <rakesh.veetekat@gmail.com>
2023-11-13 17:02:50 -0500

    Made progress on Query1.py.

--- Part1/Query_Tasks/Query1.py
+++ Part1/Query_Tasks/Query1.py
@@ -3,6 +3,26 @@ findspark.init()
 
 from pyspark import SparkConf, SparkContext
 
-conf = SparkConf.setAppName("Query1")
+# Configure Spark
+conf = SparkConf().setAppName("Query1")
 sc = SparkContext(conf=conf)
 
+# Read input file
+rddAllPeople = sc.textFile("../DataCreation/datasets/people-large.csv")
+rddInfected = sc.textFile("../DataCreation/datasets/infected-small.csv")
+
+# Filter for all the close contacts to infected people
+def f(x):
+    print(x)
+    line = x.split(",")
+    infectedID = line[0]
+    infectedX = line[1]
+    infectedY = line[2]
+
+output = rddInfected.map(f)
+
+# Save results to a file
+output.saveAsTextFile("output")
+
+# Stop Spark context
+sc.stop()
